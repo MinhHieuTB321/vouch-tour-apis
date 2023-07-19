0e58e4d5f6abdf5adee1ce118cfc7ff82d48ea9a
Minh Quang Trang <quangtm0152@gmail.com>
2023-07-19 22:32:52 +0700

    Fix

--- Apis/WebAPI/Validations/GroupCreateDTOValidation.cs
+++ Apis/WebAPI/Validations/GroupCreateDTOValidation.cs
@@ -22,7 +22,6 @@ namespace WebAPI.Validations
             RuleFor(x => x.GroupName).NotEmpty().NotNull().WithMessage("Name can not be null").WithErrorCode("400");
             RuleFor(x => x.Description).NotEmpty().NotNull().WithMessage("Name can not be null").WithErrorCode("400");
             RuleFor(x => x.Quantity).NotNull().GreaterThan(0).WithMessage("Quantity must higher than 0").WithErrorCode("400");
-            RuleFor(x => x.StartDate.AddDays(-1)).GreaterThanOrEqualTo(DateTime.Now).NotNull().WithMessage("Start Date must higher than now!").WithErrorCode("400");
             RuleFor(x => x.EndDate).GreaterThan(x => x.StartDate).NotNull().WithMessage("Start Date must higher than start Date!").WithErrorCode("400");
         }
     }
